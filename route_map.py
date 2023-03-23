d39a98e4b31ca6ca497a90b22aa9c908054c6b60
dhruvchaudhari14 <65155859+dhruvchaudhari14@users.noreply.github.com>
2023-03-23 18:16:24 +0530

    Update route_map.py

--- route_map.py
+++ route_map.py
@@ -303,7 +303,7 @@ if tab == "Optimal Solution without capacity constraints.":
         cols[1].write("Additional Data")
         
         
-results= results.to_csv(index=False).encode("utf-8")
+results = results.to_csv(index=False).encode("utf-8")
 st.write("Click to download results from the above iteration in csv format")
 st.download_button('Download', results, 'Iteration_1_results.csv', 'csv/txt')
 
@@ -428,7 +428,7 @@ if tab == "Optimal/Sub-Optimal Solution with capacity constraints.":
 
             
             
-results= results.to_csv(index=False).encode("utf-8")
+results_3= results_3.to_csv(index=False).encode("utf-8")
 st.write("Click to download results from the above iteration in csv format")
 st.download_button('Download', results_3, 'Iteration_3_results.csv', 'csv/txt')
 
