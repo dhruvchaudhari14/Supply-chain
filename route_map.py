4ba8327ce69508b372b0e71661488fe10db75f8a
dhruvchaudhari14 <65155859+dhruvchaudhari14@users.noreply.github.com>
2023-03-24 15:00:19 +0530

    Update route_map.py

--- route_map.py
+++ route_map.py
@@ -261,7 +261,7 @@ else:
 ### Iteration 1:without capacity constraint
 if tab == "Optimal Solution without capacity constraints.":
     st.write("In this section the optimization algorithm uses no constraint on warehouse capacity.")
-    
+    show_def = st.sidebar.checkbox("Use Default data",key = "data_Def")
     show_fixed_data = st.checkbox("Consider Fixed Warehouse-Market Link",key = "fixed_data_f_m_c_1")
     st.write("By using this respective markets will only get demand fulfilled by a fixed warehouse.")             
     run_code = st.checkbox("Run Iteration 1: Without Capacity Constraints")
